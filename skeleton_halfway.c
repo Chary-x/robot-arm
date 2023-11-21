bc021e019d4349cfb7629b99bb956966daecf33c
JRDMIII <revisiondami@gmail.com>
2023-11-21 15:43:16 +0000

    created all functions

--- skeleton_halfway.c
+++ skeleton_halfway.c
@@ -26,32 +26,96 @@ void wait_until_done(int connection, unsigned char id) {
 // ? Opens the grabber
 void open_grabber(int connection) { 
 	move_to_location(connection,5,0x01,0xff);
+	wait_until_done(connection,5);
 }
-
 
 // ? grabs the block
 void close_grabber(int connection) {
 	move_to_location(connection,5,0x01,0x55);
+	wait_until_done(connection,5);
+}
+
+
+void reset_arm(int connection) {
+	move_to_location(connection, 1, 0x01, 0xff);
+	move_to_location(connection, 2, 0x01, 0xff);
+	move_to_location(connection, 3, 0x01, 0xff);
+	move_to_location(connection, 4, 0x01, 0xff);
+	wait_until_done(connection, 4);
+}
+
+void lateral_movement(int connection, int pile) {
+	// todo figure out how to make it move to the right
+	unsigned int offset = pile * 0x55;
+	move_to_location(connection,1,0x01,offset);
+	wait_until_done(connection, 1);
+}
+
+void pickup(int connection, int pile, int layer) {
+	unsigned int pile_offset, layer_offset;
+
+}
+
+void LAYER_2_PILE_0(int connection) {
+	move_to_location(connection,1,0x01,0xff);
+	move_to_location(connection,2,0x01,0x11);
+	move_to_location(connection,3,0x01,0xcf);
+	move_to_location(connection,4,0x01,0x00);
+	wait_until_done(connection,4);
+}
+
+void LAYER_1_PILE_0() {
+	move_to_location(connection,1,0x01,0xff);
+	move_to_location(connection,2,0x01,0x00);
+	move_to_location(connection,3,0x01,0xcf);
+	move_to_location(connection,4,0x01,0x00);
+	wait_until_done(connection,4);
+}
+
+void LAYER_0_PILE_0() {
+	move_to_location(connection,1,0x01,0xff);
+	move_to_location(connection,2,0x01,0x00);
+	move_to_location(connection,3,0x01,0x93);
+	move_to_location(connection,4,0x01,0x50);
+	wait_until_done(connection,4);
 }
 
 int main(int argc, char* argv[]) {
+
+	// LAYER_2_PILE_0 = move_to_location(connection,1,0x01,0xff);
+	//					move_to_location(connection,2,0x01,0x11);
+	//					move_to_location(connection,3,0x01,0xcf);
+	//					move_to_location(connection,4,0x01,0x00);
+	//					wait_until_done(connection,4);
+	//
+
+	// LAYER_1_PILE_0 = move_to_location(connection,1,0x01,0xff);
+	//					move_to_location(connection,2,0x01,0x00);
+	//					move_to_location(connection,3,0x01,0xcf);
+	//					move_to_location(connection,4,0x01,0x00);
+	//					wait_until_done(connection,4);
+
+	// LAYER_0_PILE_0 = move_to_location(connection,1,0x01,0xff);
+	//					move_to_location(connection,2,0x01,0x00);
+	//					move_to_location(connection,3,0x01,0x93);
+	//					move_to_location(connection,4,0x01,0x50);
+	//
 
 	int connection = open_connection("/dev/ttyUSB0",B1000000);
 
 	wait_until_done(connection,5);
 
-	open_grabber(connection);
+	reset_arm(connection);
+	//pen_grabber(connection);
 	//close_grabber(connection);
 
-	move_to_location(connection,1,0x01,0xff);
-	move_to_location(connection,2,0x01,0x22);
-	move_to_location(connection,3,0x01,0xff);
-	move_to_location(connection,4,0x01,0x30);
-	wait_until_done(connection,4);
+	
+
+	reset_arm(connection);
+
+	//lateral_movement(connection, 1);
 
 	return 0;
-
-
 }
 
 
