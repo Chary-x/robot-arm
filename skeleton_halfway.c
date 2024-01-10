00f7f58ca8e1b4e03d5d05ec67023fdeee1e7444
itziggyb <ignatiusboateng123@gmail.com>
2024-01-10 21:15:32 +0000

    recursion sol

--- skeleton_halfway.c
+++ skeleton_halfway.c
@@ -3,111 +3,53 @@
 #include <time.h>
 #include <unistd.h>
 
-// ? This is used to move the location of a specific joint
-void move_to_location(int connection, unsigned char id,
-			unsigned char loc_h, unsigned char loc_l) {
+int disksOnPile[] = {2, -1, -1};
 
-	unsigned char cs = ~ ( id + 0x07 + 0x03 + 0x1e + loc_l + loc_h +
-				0x30 + 0x00);
+// This is used to move the location of a specific joint
+void move_to_location(int connection, unsigned char id, unsigned char loc_h,
+                      unsigned char loc_l) {
 
-	unsigned char arr[] = { 0xff, 0xff, id, 0x07, 0x03, 0x1e, loc_l,
-                                       loc_h, 0x30, 0x00, cs };
+  unsigned char cs = ~(id + 0x07 + 0x03 + 0x1e + loc_l + loc_h + 0x30 + 0x00);
 
-	int buff_len = 100;
-	unsigned char buff[buff_len];
-	
+  unsigned char arr[] = {0xff,  0xff,  id,   0x07, 0x03, 0x1e,
+                         loc_l, loc_h, 0x30, 0x00, cs};
 
-	int bytes_read = write_to_connection(connection,arr,11,buff,buff_len);
+  int buff_len = 100;
+  unsigned char buff[buff_len];
 
+  int bytes_read = write_to_connection(connection, arr, 11, buff, buff_len);
 }
 
-void wait_until_done(int time) {
-	usleep(time);
+void wait_until_done(int time) { usleep(time); }
+
+// Opens the grabber
+void openGrabber(int connection) {
+  move_to_location(connection, 5, 0x01, 0xff);
+  wait_until_done(2000000);
 }
 
-// ? Opens the grabber
-void openGrabber(int connection) { 
-	move_to_location(connection,5,0x01,0xff);
-	wait_until_done(2000000);
-}
-
-// ? Closes the grabber on the block
+// Closes the grabber on the block
 void closeGrabber(int connection) {
-	move_to_location(connection,5,0x01,0x35);
-	wait_until_done(2000000);
+  move_to_location(connection, 5, 0x01, 0x35);
+  wait_until_done(2000000);
 }
 
-// ? This reset the arm back to the upright position
+// This reset the arm back to the upright position
 void resetArm(int connection) {
-	move_to_location(connection, 2, 0x01, 0xff);
-	move_to_location(connection, 3, 0x01, 0xff);
-	move_to_location(connection, 4, 0x01, 0xff);
-	wait_until_done(2000000);
+  move_to_location(connection, 2, 0x01, 0xff);
+  move_to_location(connection, 3, 0x01, 0xff);
+  move_to_location(connection, 4, 0x01, 0xff);
+  wait_until_done(2000000);
 }
 
-// ? Performs movement ot the different piles
+// Performs movement to different piles
 void lateralMovement(int connection, int pile) {
-	unsigned int piles[] = {0xff, 0x95, 0x35};
-	move_to_location(connection, 1, 0x01, piles[pile]);
-	wait_until_done(2000000);
+  unsigned int piles[] = {0xff, 0x95, 0x35};
+  move_to_location(connection, 1, 0x01, piles[pile]);
+  wait_until_done(2000000);
 }
 
-// Moves the arm first over the correct pile then the correct layer
-void pickup(int connection, int layer, int pile) {
-	// Resetting the arm to neutral
-	resetArm(connection);
-
-	// Making sure the grabber is open
-	openGrabber(connection);
-
-	// Moving arm over correct pile
-	lateralMovement(connection, pile);
-
-	// Moving the arm to the correct layer
-	switch(layer) {
-		case 0: layer0(connection, 0x00); break;
-		case 1: layer1(connection, 0x00); break;
-		default: layer2(connection, 0x00); break;
-	}
-
-	wait_until_done(3000000);
-	// Grip the block
-	closeGrabber(connection);
-	wait_until_done(4000000);
-
-	// Return the neutral position
-	resetArm(connection);
-}
-
-// Moves the arm first over the correct pile then the correct layer
-void dropoff(int connection, int layer, int pile) {
-	// Resetting the arm to neutral
-	resetArm(connection);
-
-	// Moving arm over correct pile
-	lateralMovement(connection, pile);
-
-
-	// Moving the arm to the correct layer
-	switch(layer) {
-		case 0: layer0(connection, 0); break;
-		case 1: layer1(connection, 0); break;
-		default: layer2(connection, 0x10); break;
-	}
-
-
-
-	wait_until_done(3000000);
-	
-
-	// Grip the block
-	openGrabber(connection);
-	wait_until_done(1000000);
-
-	// Return the neutral position
-	resetArm(connection);
-}
-
+// Performs movement to different layers
 void verticalMovement(int connection, int layer) {
   unsigned int layers[3][3] = {
       {0x30, 0x9a, 0x00}, {0x10, 0xb0, 0x00}, {0x00, 0xaa, 0x00}};
@@ -118,57 +60,79 @@ void verticalMovement(int connection, int layer) {
   }
 }
 
-int main(int argc, char* argv[]) {
-	// ? Do we make the connection a global variable?
-	int connection = open_connection("/dev/ttyUSB0",B1000000);
+// Moves the arm first over the correct pile then the correct layer
+void pickup(int connection, int from) {
+  // Resetting the arm to neutral
+  resetArm(connection);
 
-	openGrabber(connection);
-	/* 
-	? The sequence of movements we need to make
-	! Start !
-	* Step 0: Layer 2 Pile 0 -> Layer 0 Pile 2
-	* Step 1: Layer 1 Pile 0 -> Layer 0 Pile 1
-	* Step 2: Layer 0 Pile 2 -> Layer 2 Pile 1
-	* Step 3: Layer 0 Pile 0 -> Layer 0 Pile 2
-	* Step 4: Layer 1 Pile 1 -> Layer 0 Pile 0
-	* Step 5: Layer 0 Pile 1 -> Layer 1 Pile 2
-	* Step 6: Layer 0 Pile 0 -> Layer 2 Pile 2
-	! Done !
-	*/ 
-	resetArm(connection);
-	
-	// Step 0
-	pickup(connection, 2, 0);
-	dropoff(connection, 0, 2);
+  // Making sure the grabber is open
+  openGrabber(connection);
 
-	// Step 1
-	pickup(connection, 1, 0);
-	dropoff(connection, 0, 1);
+  // Moving arm over correct pile
+  lateralMovement(connection, from);
 
-	// Step 2
-	pickup(connection, 0, 2);
-	dropoff(connection, 2, 1);
+  // Moving the arm to the correct layer
+  verticalMovement(connection, disksOnPile[from]);
 
-	// Step 3
-	pickup(connection, 0, 0);
-	dropoff(connection, 0, 2);
+  wait_until_done(3000000);
+  // Grip the block
+  closeGrabber(connection);
+  wait_until_done(4000000);
 
-	// Step 4 
-	pickup(connection, 1, 1);
-	dropoff(connection, 0, 0);
-
-	// Step 5
-	pickup(connection, 0, 1);
-	dropoff(connection, 1, 2);
-
-	// Step 6
-	pickup(connection, 0, 0);
-	dropoff(connection, 2, 2);
-
-	return 0;
+  // Return the neutral position
+  resetArm(connection);
 }
 
+void dropoff(int connection, int to) {
+  // Resetting the arm to neutral
+  resetArm(connection);
 
+  // Moving arm over correct pile
+  lateralMovement(connection, to);
 
+  // Moving the arm to the correct layer
+  verticalMovement(connection, disksOnPile[to]);
 
+  wait_until_done(3000000);
 
+  // Grip the block
+  openGrabber(connection);
+  wait_until_done(1000000);
+
+  // Return the neutral position
+  resetArm(connection);
+}
+
+// Updates appropriate state regarding amount of disks on each pile
+void updateDisksOnPiles(int connection, int from, int to) {
+  disksOnPile[to]++;
+
+  pickup(connection, from);
+  dropoff(connection, to);
+
+  disksOnPile[from]--;
+}
+
+// This is the recursive algorithm that calculates which piles and layers to
+// move to
+char towerOfHanoi(int connection, int n, int from, int to, int aux) {
+  if (n == 0) {
+    return 'D';
+  }
+
+  towerOfHanoi(n - 1, from, aux, to);
+  updateDisksOnPiles(from, to);
+  towerOfHanoi(n - 1, aux, to, from);
+}
+
+// Main program flow
+int main(void) {
+  int connection = open_connection("/dev/ttyUSB0", B1000000);
+
+  // Ensuring robot is in neutral state before starting
+  openGrabber(connection);
+  resetArm(connection);
+
+  towerOfHanoi(connection, 3, 0, 2, 1);
+  return 0;
+}
